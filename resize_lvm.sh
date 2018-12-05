41f16db16f1ac77cd31f5be2dfa14a812f74af6f
mosyae <33257923+mosyae@users.noreply.github.com>
2018-12-05 14:29:38 +0200

    Update resize_lvm.sh

--- resize_lvm.sh
+++ resize_lvm.sh
@@ -1,5 +1,8 @@
  #Add space to the virtual disk in VMWare (can be done on-line)
  #Create VMWare snapshot - please note that you will be unable to change disk size with snapshot, so create snapshot after you changed disk size
+ # Rescan disk to see the added space:
+ echo 1>/sys/class/block/sda/device/rescan
+ #Check that the disk now is seen by PS as a new size:
  fdisk -l
  # Create a new pripary partition that will be used to extend logical volume 
  fdisk /dev/sda
@@ -24,9 +27,11 @@
  vgdisplay
  # Now see the all Logical Volumes names - you will need to logical volume extend command
  lvdisplay
- Check all physical vloumes with the disks
+ #Check all physical vloumes with the disks
  pvscan
  # Now expand the logical volume
+ lvextend -l +100%FREE /dev/cl_sfws-gen/root
+ #O R 
  lvextend -L+39G /dev/cl_sfws-gen/root
  # Check that the size is not added to the / root file system mount point
  df -h
